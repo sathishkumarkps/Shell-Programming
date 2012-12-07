89f247e57ca7e948c97ce5cbe130daa23a12812a
sathishkumarkps <sathishkumarkps@outlook.com>
2012-12-07 05:04:32 -0800

    Basic Firewall

---
+++ basic-iptable-rule.sh
@@ -0,0 +1,14 @@
+#Basic Iptable entries 
+
+# Block Brute Force Attack
+
+/sbin/iptables -A INPUT -p tcp --dport 22 -m state --state NEW -m recent --update --seconds 60 --hitcount 4 -j DROP
+/sbin/iptables -A INPUT -p tcp --dport 22 -m state --state NEW -m recent --set
+/sbin/iptables -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT
+
+# Allow HTTP and HTTPS
+
+/sbin/iptables -A INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
+
+# 
